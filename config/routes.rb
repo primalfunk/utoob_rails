e8edfd154ae5c3dc4ce8b85c9bb55099773d8d2a
Jared Menard <jared.d.menard@gmail.com>
2018-06-02 11:50:32 -0600

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
- root 'movie#index'
+ root 'movies#index'
+ devise_for :users
 
  resources :users do
    resources :comments
@@ -8,9 +9,5 @@ Rails.application.routes.draw do
  resources :movies do
    resources :comments
  end
-
-  resources :movies do
-    resources :comments
-  end
-  
+ 
 end
