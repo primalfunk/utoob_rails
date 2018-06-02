5da3a9242157d2e452aa849101f1c0cf3eea0cac
Jared Menard <jared.d.menard@gmail.com>
2018-06-02 11:16:05 -0600

    comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,56 @@
 class CommentsController < ApplicationController
+  before_action :set_movie
+  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+
   def index
+    @comments = @movie.comments
   end
 
   def show
   end
 
   def new
+    @comment = @movie.comments.new
+    render partial: "form"
   end
 
   def edit
+    render partial: "form"
   end
+
+  def create
+    @comment = @movie.comments.new(comment_params)
+
+    if @comment.save
+      redirect_to [@movie, @comment]
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to [@movie, @comment]
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to movie_comments_path
+  end
+
+  private
+    def set_movie
+      @movie = Movie.find(params[:movie_id])
+    end
+
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
 end
