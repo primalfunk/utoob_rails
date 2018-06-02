bb01b82a95f89379d22ac443cd5380bcc780e9ec
Alan Bischoff <alanlbischoff@gmail.com>
2018-06-02 10:09:00 -0600

    seed file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'movies/index'
-  get 'movies/new'
-  get 'movies/show'
-  get 'movies/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
