b2d7a49f372b7bc2edf77c218fff54c2f37ca178
Jared Menard <jared.d.menard@gmail.com>
2018-06-02 10:37:34 -0600

    movie controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,49 @@
 class MoviesController < ApplicationController
+  before_action :set_movie
+  
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def edit
+
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to movies_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @movie.update(movie_params)
+      redirect_to movies_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @movie.destroy
+  end
+
+  private
+    def set_movie
+      @movie = Movie.find(params[:id])
+    end
+  
+    def movie_params
+      params.require(:movie).permit(:title, :duration, :genre, :description, :trailer)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
- root 'movie#index'
+ root 'movies#index'
 
  resource :users do
    resource :comments
