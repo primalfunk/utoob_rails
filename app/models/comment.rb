17a5e59daf73fc092cdd63b658b55d3351389cb7
Alan Bischoff <alanlbischoff@gmail.com>
2018-06-02 13:29:15 -0600

    commit add link

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
   belongs_to :movie
   belongs_to :user, optional: true
+  before_save :check_author
+
+ def check_author
+   if self.user.blank?
+     self.user = 'anon'
+   end
+ end
+end
 end
